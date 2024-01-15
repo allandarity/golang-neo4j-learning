b42da6b6e6b9c1411c6dcc4b7571cdd232673146
xdesign-elliottlewandowski <elliott.lewandowski@xdesign.com>
2024-01-15 11:54:15 +0000

    add a bunch of stuff for testing

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	config := &neo4jConfiguration{
 		url:      "bolt://localhost:7687",
 		username: "neo4j",
-		password: "test1234",
+		password: "password",
 		database: "",
 	}
 
@@ -32,14 +32,88 @@ func main() {
 		fmt.Println(err)
 	}
 	defer driver.Close(ctx)
+	//matchAll(driver, config.database, "Person")
+	example(driver)
+}
 
-	result, err := neo4j.ExecuteQuery(
-		ctx, driver, "testststest", map[string]interface{}{"limit": 5}, neo4j.EagerResultTransformer, neo4j.ExecuteQueryWithReadersRouting(), neo4j.ExecuteQueryWithDatabase(config.database))
+func matchAll(driver neo4j.DriverWithContext, database string, node string) {
+	queryString := fmt.Sprintf("MATCH (n: %s) return *", node)
+	result, err := neo4j.ExecuteQuery(context.Background(), driver, queryString, nil, neo4j.EagerResultTransformer,
+		neo4j.ExecuteQueryWithDatabase(database), neo4j.ExecuteQueryWithReadersRouting())
 
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(result)
 
-	fmt.Println("hello")
+	for _, record := range result.Records {
+		fmt.Println(record.AsMap())
+		x, _ := record.Get("from")
+		fmt.Println(x)
+	}
+}
+
+func example(driver neo4j.DriverWithContext) {
+	ctx := context.Background()
+	// Prepare data
+	people := []map[string]any{
+		{"name": "Alice", "age": 42, "friends": []string{"Bob", "Peter", "Anna"}},
+		{"name": "Bob", "age": 19},
+		{"name": "Peter", "age": 50},
+		{"name": "Anna", "age": 30},
+	}
+
+	// Create some nodes
+	for _, person := range people {
+		_, err := neo4j.ExecuteQuery(ctx, driver,
+			"MERGE (p:Person {name: $person.name, age: $person.age})",
+			map[string]any{
+				"person": person,
+			}, neo4j.EagerResultTransformer,
+			neo4j.ExecuteQueryWithDatabase("neo4j"))
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Create some relationships
+	for _, person := range people {
+		if person["friends"] != "" {
+			_, err := neo4j.ExecuteQuery(ctx, driver, `
+                MATCH (p:Person {name: $person.name})
+                UNWIND $person.friends AS friend_name
+                MATCH (friend:Person {name: friend_name})
+                MERGE (p)-[:KNOWS]->(friend)
+                `, map[string]any{
+				"person": person,
+			}, neo4j.EagerResultTransformer,
+				neo4j.ExecuteQueryWithDatabase("neo4j"))
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	// Retrieve Alice's friends who are under 40
+	result, err := neo4j.ExecuteQuery(ctx, driver, `
+        MATCH (p:Person {name: $name})-[:KNOWS]-(friend:Person)
+        WHERE friend.age < $age
+        RETURN friend
+        `, map[string]any{
+		"name": "Alice",
+		"age":  40,
+	}, neo4j.EagerResultTransformer,
+		neo4j.ExecuteQueryWithDatabase("neo4j"))
+	if err != nil {
+		panic(err)
+	}
+
+	// Loop through results and do something with them
+	for _, record := range result.Records {
+		fmt.Println(record.AsMap())
+	}
+
+	// Summary information
+	fmt.Printf("\nThe query `%v` returned %v records in %+v.\n",
+		result.Summary.Query().Text(), len(result.Records),
+		result.Summary.ResultAvailableAfter())
 }
